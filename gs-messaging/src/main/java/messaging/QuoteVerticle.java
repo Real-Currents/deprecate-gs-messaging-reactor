cf656e5cae66799b3d4fae9c51fc80f0a5614b2a
John <john@real-currents.com>
2017-03-27 10:04:37 -0400

    db connection in verticle

--- gs-messaging/src/main/java/messaging/QuoteVerticle.java
+++ gs-messaging/src/main/java/messaging/QuoteVerticle.java
@@ -3,6 +3,10 @@ package messaging;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.MessageConsumer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.asyncsql.AsyncSQLClient;
+import io.vertx.ext.asyncsql.PostgreSQLClient;
+import io.vertx.ext.sql.SQLConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,14 +19,35 @@ public class QuoteVerticle extends AbstractVerticle {
     @Autowired
     EventBus eventBus;
 
+    private JsonObject postgresConfig = new JsonObject("{ " +
+            "\"host\" : \"localhost\", " +
+            "\"port\" : 5432, " +
+            "\"maxPoolSize\" : 10, " +
+            "\"username\" : \"reactive\", " +
+            "\"password\" : \"reactive\", " +
+            "\"database\" : \"spring_quotes\"" +
+        " }");
+    private AsyncSQLClient postgresClient;
+    private MessageConsumer<String> quoteRequestListener;
+
     // Synchronous verticle start routine
     public void start () {
-        MessageConsumer<String> quoteRequestListener = eventBus.consumer("quote.request");
-
         System.err.println("Verticle Deployed");
 
+        postgresClient = PostgreSQLClient.createShared(vertx, postgresConfig);
+
+        quoteRequestListener = eventBus.consumer("quote.request");
         quoteRequestListener.handler(message -> {
             System.out.println( "Quote requested: "+ message.body());
+
+            postgresClient.getConnection(res -> {
+                if (res.succeeded()) {
+                    System.err.println("Connected to quote db.");
+                    SQLConnection postgresConnection = res.result();
+                } else {
+                    System.err.println("Failed to connect to db: "+ res.cause());
+                }
+            });
         });
 
     }
